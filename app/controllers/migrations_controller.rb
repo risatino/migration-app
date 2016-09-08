ea3bde2c8e0bed9b3144cb2d00799a8eb81a2fa5
risatino <charissatolentino@gmail.com>
2016-09-07 21:35:12 -0500

    added a controller

---
+++ app/controllers/migrations_controller.rb
@@ -0,0 +1,2 @@
+class MigrationsController < ApplicationController
+end
---
+++ test/controllers/migrations_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MigrationsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
